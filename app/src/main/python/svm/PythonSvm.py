dc431c7732536fd2bf75eab8e1cb8ebb9f0d0926
Fredrik Sejr <swashb0ckler@protonmail.com>
2023-04-11 12:47:27 +0200

    feature: updated airbnb csv with frequencies

--- app/src/main/python/svm/PythonSvm.py
+++ app/src/main/python/svm/PythonSvm.py
@@ -1,7 +1,18 @@
+import pandas as pd
+
+# Import data
+df = pd.read_csv('app/src/main/resources/listings.csv')
 
 # Label the data (1) (label running frequencies)
+df['actual_frequency'] = 0
+
+df['actual_frequency'] = df.groupby('neighbourhood').cumcount() + 1
+
+df.to_csv('app/src/main/resources/updated_listings.csv', index=False)
+
 
 # Label the data (2) (label heavy hitters)
+
 
 # SVM on labelled data
 
