f893da266cfa25e375fe06b80229ba3979db08fa
Fredrik Sejr <swashb0ckler@protonmail.com>
2023-04-12 08:49:34 +0200

    feature: heavy_hitter column (fixed data size)

--- app/src/main/python/svm/PythonSvm.py
+++ app/src/main/python/svm/PythonSvm.py
@@ -3,16 +3,35 @@ import pandas as pd
 # Import data
 df = pd.read_csv('app/src/main/resources/listings.csv')
 
+'''
+THIS USES running frequency for dynamic labelling
+
 # Label the data (1) (label running frequencies)
 df['actual_frequency'] = 0
 
 df['actual_frequency'] = df.groupby('neighbourhood').cumcount() + 1
 
 df.to_csv('app/src/main/resources/updated_listings.csv', index=False)
+'''
+
+# Label the data (1) (label fixed frequencies)
+
+df['actual_frequency'] = 0
+
+df['actual_frequency'] = df.groupby('neighbourhood')['neighbourhood'].transform('count')
 
 
-# Label the data (2) (label heavy hitters)
+# Label the data (2) (label heavy hitters) - OBS this is a fixed dataset size for training
 
+l1Norm = len(df['neighbourhood'])
+epsilon = 0.01
+threshold = l1Norm * epsilon
+
+df['heavy_hitter'] = df['actual_frequency'].apply(lambda x: 'heavy' if x > threshold else 'not_heavy')
+
+df.to_csv('app/src/main/resources/updated_listings.csv', index=False)
+
+#print(threshold)
 
 # SVM on labelled data
 
